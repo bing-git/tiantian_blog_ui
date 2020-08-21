e7182a07e3a3aef8cea065ca67abe468da3990ce
qibing <1468550371@qq.com>
2020-08-21 08:43:03 +0800

    前端动态路由加载

---
+++ src/api/router.js
@@ -0,0 +1,8 @@
+import request from '@/utils/request'
+
+export function getRouter() {
+  return request({
+    method: 'post',
+    url: '/base/router/'
+  })
+}
---
+++ src/store/modules/router.js
@@ -0,0 +1,54 @@
+import { getRouter } from '@/api/router'
+
+const state = {
+  routers: [],
+  menus: []
+}
+
+const mutations = {
+  SET_ROUTERS: (state, routers) => {
+    state.routers = routers
+  },
+  SET_MENUS: (state, menus) => {
+    state.menus = menus
+  }
+}
+
+function filterAsyncRouter(asyncRouterMap) {
+  return asyncRouterMap.filter(route => {
+    if (route.children && route.children.length) {
+      route.children = filterAsyncRouter(route.children)
+    }
+    if (!route.hidden) {
+      return route
+    }
+  })
+}
+
+const actions = {
+  getRouters({ commit }) {
+    return new Promise((resolve, reject) => {
+      getRouter()
+        .then(res => {
+          // console.log(res)
+          const { data } = res
+          commit('SET_ROUTERS', data)
+          // 去除hidden===1的项目
+          const newObj = JSON.parse(JSON.stringify(data))
+          const menus = filterAsyncRouter(newObj)
+          commit('SET_MENUS', menus)
+          resolve(data)
+        })
+        .catch(error => {
+          reject(error)
+        })
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
